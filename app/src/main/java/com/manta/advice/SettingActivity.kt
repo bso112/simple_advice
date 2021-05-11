0bacf363687b00fc15a718f508b17adf4aa4706b
Byeon <manta11246@gmail.com>
2021-05-11 16:07:41 +0900

    테마에서 기본 컬러 가져오기 구현중

--- app/src/main/java/com/manta/advice/SettingActivity.kt
+++ app/src/main/java/com/manta/advice/SettingActivity.kt
@@ -1,10 +1,14 @@
 package com.manta.advice
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.RadioButton
+import androidx.annotation.ColorRes
+import androidx.annotation.StyleRes
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.content.res.use
 import androidx.core.view.get
@@ -30,28 +34,26 @@ class SettingActivity : AppCompatActivity() {
     }
 
     private fun ActivitySettingBinding.addThemeSelectButtons() {
-        createThemeSelectButtons(listOf(R.style.blackAndWhite, R.style.orange)).forEach {
+        createThemeSelectButtons(R.style.blackAndWhite, R.style.orange).forEach {
             themeGroup.addView(it)
         }
     }
 
-    private fun createThemeSelectButtons(themeList: List<Int>): List<RadioButton> {
+    private fun createThemeSelectButtons(@StyleRes vararg themeList: Int): List<RadioButton> {
         val buttons = mutableListOf<RadioButton>()
         for (theme in themeList) {
             RadioButton(this)
                 .apply {
                     buttonDrawable = null
-                    background = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_brightness_1_24, this@SettingActivity.theme)
+                    background = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_brightness_1_24, null)
                     layoutParams = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
                     setOnClickListener {
                         application.setTheme(theme)
                     }
+                    //TODO : colorPrimary를 받아와야 되는데..
                     obtainStyledAttributes(theme, intArrayOf(android.R.attr.colorPrimary)).use {
-                        backgroundTintList = ResourcesCompat.getColorStateList(
-                            resources,
-                            it.getResourceId(0, R.color.black),
-                            this@SettingActivity.theme
-                        )
+                        Log.d("manta", "컬러 : " +  java.lang.Integer.toHexString(it.getColor(0, Color.BLACK)))
+
                     }
                 }.also { buttons.add(it) }
         }
